3b4025c1c0ebf3cb396aa1826de6029b1d5a68ab
dtk <dtk@gmx.de>
2011-10-26 00:31:59 +0200

    Add installation mechanism
    This commit fixes issue #11 and prepares issue #12.
    The README has been adjusted accordingly.

---
+++ treasurer/__init__.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+#
+# Copyright (C) dtk <dtk@gmx.de>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+__productname__ = 'treasurer'
+__version__ = '0.1-dev'
+__copyright__ = "Copyright (C) 2011 dtk"
+__author__ = "dtk"
+__author_email__ = "dtk@gmx.de"
+__description__ = "The treasurer commands access to your gnome keyring daemon"
+__url__ = "https://github.com/0x64746b/treasurer"
+
